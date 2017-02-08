7c9611cc22322a467b56a826af7784c5b77df45a
Gordon Lesti <info@gordonlesti.com>
2017-02-08 09:54:44 +0100

    phpunit 6.0

--- tests/LevenshteinTest.php
+++ tests/LevenshteinTest.php
@@ -4,75 +4,37 @@ declare(strict_types=1);
 namespace GordonLesti\LevenshteinTest;
 
 use GordonLesti\Levenshtein\Levenshtein;
+use PHPUnit\Framework\TestCase;
 
 /**
- * @SuppressWarnings(PHPMD.TooManyPublicMethods)
  * @SuppressWarnings(PHPMD.StaticAccess)
  */
-class LevenshteinTest extends \PHPUnit_Framework_TestCase
+class LevenshteinTest extends TestCase
 {
-    public function testGetDistanceDefaultCost()
+    /**
+     * @dataProvider levenshteintProvider
+     */
+    public function testLevenshtein(array $args, $result)
     {
-        $this->assertSame(0.0, Levenshtein::levenshtein("", ""));
+        $this->assertSame($result, Levenshtein::levenshtein(...$args));
     }
 
-    public function testGetDistanceL0L4()
+    public function levenshteintProvider(): array
     {
-        $this->assertSame(4.0, Levenshtein::levenshtein("", "AAAB"));
-    }
-
-    public function testGetDistanceL4L0()
-    {
-        $this->assertSame(4.0, Levenshtein::levenshtein("CACC", ""));
-    }
-
-    public function testGetDistanceL4L4()
-    {
-        $this->assertSame(2.0, Levenshtein::levenshtein("ABCA", "BBCB"));
-    }
-
-    public function testGetDistanceL2L4()
-    {
-        $this->assertSame(3.0, Levenshtein::levenshtein("AC", "ABAA"));
-    }
-
-    public function testGetDistanceL4L2()
-    {
-        $this->assertSame(2.0, Levenshtein::levenshtein("CAAA", "AA"));
-    }
-
-    public function testGetDistanceL0L0I2R9D5()
-    {
-        $this->assertSame(0.0, Levenshtein::levenshtein("", "", 2, 9, 5));
-    }
-
-    public function testGetDistanceL0L4I8R3D2()
-    {
-        $this->assertSame(32.0, Levenshtein::levenshtein("", "BBBB", 8, 3, 2));
-    }
-
-    public function testGetDistanceL4L0I3R7D2()
-    {
-        $this->assertSame(8.0, Levenshtein::levenshtein("BACA", "", 3, 7, 2));
-    }
-
-    public function testGetDistanceL4L4I9R4D8()
-    {
-        $this->assertSame(8.0, Levenshtein::levenshtein("CCAA", "BCCA", 9, 4, 8));
-    }
-
-    public function testGetDistanceL4L2I7R9D2()
-    {
-        $this->assertSame(13.0, Levenshtein::levenshtein("ACCB", "BC", 7, 9, 2));
-    }
-
-    public function testGetDistanceL2L4I1R4D6()
-    {
-        $this->assertSame(6.0, Levenshtein::levenshtein("CA", "AABC", 1, 4, 6));
-    }
-
-    public function testGetDistanceL4L4UTF8()
-    {
-        $this->assertSame(3.0, Levenshtein::levenshtein("♫⚓⚓♥", "♫♥⚓♫⚓♫"));
+        return [
+            [["", ""], 0.0],
+            [["", "AAAB"], 4.0],
+            [["CACC", ""], 4.0],
+            [["ABCA", "BBCB"], 2.0],
+            [["AC", "ABAA"], 3.0],
+            [["CAAA", "AA"], 2.0],
+            [["", "", 2, 9, 5], 0.0],
+            [["", "BBBB", 8, 3, 2], 32.0],
+            [["BACA", "", 3, 7, 2], 8.0],
+            [["CCAA", "BCCA", 9, 4, 8], 8.0],
+            [["ACCB", "BC", 7, 9, 2], 13.0],
+            [["CA", "AABC", 1, 4, 6], 6.0],
+            [["♫⚓⚓♥", "♫♥⚓♫⚓♫"], 3.0],
+        ];
     }
 }
